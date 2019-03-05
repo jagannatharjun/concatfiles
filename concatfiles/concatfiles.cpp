8abd4018a281f2a93abf73ab6eaaf687c8445ccc
Prince Gupta <jagannatharjun@github.com>
2019-03-05 22:49:01 +0530

    Add project files.

---
+++ concatfiles/concatfiles.cpp
@@ -0,0 +1,12 @@
+﻿// concatfiles.cpp : Defines the entry point for the application.
+//
+
+#include "concatfiles.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
